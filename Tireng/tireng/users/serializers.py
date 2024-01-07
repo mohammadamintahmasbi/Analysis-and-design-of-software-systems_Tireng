eb37a754398a947ce62a382515a3b0039e66d474
mohammad amin <you@example.com>
2024-01-07 17:41:15 +0330

    add one comment

--- Tireng/tireng/users/serializers.py
+++ Tireng/tireng/users/serializers.py
@@ -7,6 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["email", "username", "password"]
 
+    # set_password use to save password hashed in db
     def create(self, validate_data):
         user = User(email=validate_data["email"])
         user.set_password(validate_data["password"])
