fba7f7ba8e1d06630bf180b912ed603dcce8aeea
mohammad amin <you@example.com>
2024-01-09 15:23:14 +0330

    fix little problem

--- Tireng/tireng/village/models.py
+++ Tireng/tireng/village/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from tireng.users.models import User
 
 class Village(models.Model):
-    name = models.CharField(max_lenght=64)
+    name = models.CharField(max_length=64)
     image = models.ImageField(null=True, blank=True, upload_to="tireng/media/village_img")
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_lenght=200)
+    address = models.CharField(max_length=200)
     num_of_rooms = models.PositiveIntegerField(null=True, blank=True)
     meterage = models.PositiveIntegerField(null=True, blank=True)
     price_per_day = models.PositiveIntegerField()
