c39c12db542ae2952c4b4ae5318e8961058f83e2
mohammad amin <you@example.com>
2024-01-09 15:03:50 +0330

    create village model without migrations

--- Tireng/tireng/village/models.py
+++ Tireng/tireng/village/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from tireng.users.models import User
 
-# Create your models here.
+class Village(models.Model):
+    name = models.CharField(max_lenght=64)
+    image = models.ImageField(null=True, blank=True, upload_to="tireng/media/village_img")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_lenght=200)
+    num_of_rooms = models.PositiveIntegerField(null=True, blank=True)
+    meterage = models.PositiveIntegerField(null=True, blank=True)
+    price_per_day = models.PositiveIntegerField()
+    description = models.TextField()
+
+    
