b35fe548a352af65fad7250797f282435ac9c75f
mohammad amin <mat.chenari.computer@gmail.com>
2024-01-20 20:49:48 +0330

    add is_owner field to signup api

--- Tireng/tireng/users/serializers.py
+++ Tireng/tireng/users/serializers.py
@@ -5,12 +5,12 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "username", "password"]
+        fields = ["email", "is_owner", "password"]
 
     # set_password use to save password hashed in db
     def create(self, validate_data):
         user = User(email=validate_data["email"])
         user.set_password(validate_data["password"])
-        user.username = validate_data["username"]
+        user.is_owner = validate_data["is_owner"]
         user.save()
         return user
